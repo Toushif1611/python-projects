23a59b90c9a2ab5eeb4912ccd41db3a0c1243131
toushifA <toushif1611@gamil.com>
2023-11-15 15:57:25 +0530

    Add Initial Commit

---
+++ Turtle_run/Turtle_run4.py
@@ -0,0 +1,90 @@
+# Turtle run
+# by ToushifA_1611
+# check food collision with player
+
+import turtle
+import math
+import random
+ 
+speed=1
+
+#set screen
+s=turtle.Screen()
+s.bgcolor("black")
+s.title("TURTLE-RUN")
+
+#boarder
+boarder_pen = turtle.Turtle()
+boarder_pen.color("white")
+boarder_pen.pensize(3)
+boarder_pen.penup()
+boarder_pen.setposition(-300,-300)
+boarder_pen.pendown()
+for i in range(4):
+    boarder_pen.forward(600)
+    boarder_pen.left(90)
+boarder_pen.hideturtle()
+
+#player
+player=turtle.Turtle()
+player.shape("turtle")
+player.color("green")
+player.speed(0)
+player.penup()
+
+#food
+food=turtle.Turtle()
+food.shape("circle")
+food.color("red")
+food.speed(0)
+food.penup()
+food.setposition(random.randint(-290, 290), random.randint(-290, 290))
+
+
+#function
+def turnleft():
+    player.left(30)
+
+def turnright():
+    player.right(30)
+
+def increasespeed():
+    global speed
+    speed+=1
+
+def decreasespeed():
+    global speed
+    speed-=1
+
+def isCollision(t1, t2):
+    d = math.sqrt(math.pow(t1.xcor()-t2.xcor(),2) + math.pow(t1.ycor()-t2.ycor(),2))
+    if d <20:
+        return True
+    else:
+        return False
+    
+#key binding
+s.listen()
+s.onkeypress(turnleft,"Left")
+s.onkeypress(turnright,"Right")
+s.onkeypress(increasespeed,"Up")
+s.onkeypress(decreasespeed,"Down")  
+
+#mainloop
+while True:
+    
+    #player move
+    player.forward(speed)
+
+    #boarder collision
+    if player.xcor()> 300 or player.xcor()< -300:
+        player.right(100)
+    if player.ycor()> 300 or player.ycor()< -300:
+        player.right(100)
+
+    #collision checking
+    if isCollision(player, food):
+        food.setposition(random.randint(-290, 290), random.randint(-290, 290))
+
+
+s.mainloop()
