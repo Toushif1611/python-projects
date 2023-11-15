8c7f2873dc57a86c497dd6d02b9ded1e892ae7a3
toushifA <toushif1611@gamil.com>
2023-11-15 13:22:51 +0530

    Add Initial Commit

--- space-invader/space_invader2.py
+++ space-invader/space_invader2.py
@@ -12,9 +12,11 @@ s.title("Space Invader")
 #boarder
 boarder_pen = turtle.Turtle()
 boarder_pen.color("white")
+boarder_pen.speed(0)
 boarder_pen.penup()
-boarder_pen.goto(-300,-300)
+boarder_pen.setposition(-300,-300)
 boarder_pen.pendown()
+boarder_pen.pensize(3)
 for i in range(4):
     boarder_pen.fd(600)
     boarder_pen.lt(90)
@@ -25,12 +27,30 @@ player = turtle.Turtle()
 player.shape("triangle")
 player.color("blue")
 player.penup()
-player.setposition(0,-280)
+player.speed(0)
+player.setposition(0,-250)
 player.setheading(90)
 
+playerspeed =15
+
+#function
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if  x < -280:
+        x = -280
+    player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if  x > 280:
+        x = 280
+    player.setx(x)
 
 
-
-
+s.listen()
+s.onkeypress(move_left,"Left")
+s.onkeypress(move_right,"Right")
 
 s.mainloop()
