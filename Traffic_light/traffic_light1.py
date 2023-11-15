273cc5751a2c4c0f9ca34fada854abc203e5d0fa
toushifA <toushif1611@gamil.com>
2023-11-15 15:11:05 +0530

    Add Initial Commit

--- Traffic_light/traffic_light1.py
+++ Traffic_light/traffic_light1.py
@@ -12,20 +12,20 @@ s.title("traffic-light")
 s.setup(width=300, height=300)
 
 #boarder
-pen=turtle.Turtle()
-pen.color("brown")
-pen.speed(0)
-pen.penup()
-pen.goto(-20, -50)
-pen.pendown()
-pen.pensize(3)
-pen.hideturtle()
+boarder_pen=turtle.Turtle()
+boarder_pen.color("brown")
+boarder_pen.speed(0)
+boarder_pen.penup()
+boarder_pen.goto(-20, -50)
+boarder_pen.pendown()
+boarder_pen.pensize(3)
+boarder_pen.hideturtle()
 for i in range(4):
-    pen.forward(40)
-    pen.left(90)
-    pen.forward(100)
-    pen.left(90)
-    
+    boarder_pen.forward(40)
+    boarder_pen.left(90)
+    boarder_pen.forward(100)
+    boarder_pen.left(90)
+
 #Red lights
 red=turtle.Turtle()
 red.shape("circle")
